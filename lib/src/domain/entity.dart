e8bef52a8153ec640b34a8cdd5bc4e726029a0c0
huwentao <hu.wentao@outlook.com>
2022-02-11 13:23:10 +0800

    remove entity.dart IEntity

--- lib/src/domain/entity.dart
+++ lib/src/domain/entity.dart
@@ -3,14 +3,6 @@
 // Date  : 2020/3/24
 // Time  : 8:31
 
-import 'package:get_arch_core/get_arch_core.dart';
-
-///
-/// 实体类实现本接口
-abstract class IEntity<Id extends Identity> {
-  Id get id;
-}
-
 ///
 /// 无Identify
 abstract class IEtt<Id> {
