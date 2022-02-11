91c768a2a5eb92a8e6407a40f9b5937e57f4ec36
huwentao <hu.wentao@outlook.com>
2022-02-11 13:23:52 +0800

    refactor repository.dart class to mixin

--- lib/src/domain/repository.dart
+++ lib/src/domain/repository.dart
@@ -7,41 +7,14 @@ import 'package:get_arch_core/get_arch_core.dart';
 
 ///
 /// 仓库抽象类
-/// 建议缩写为: Repo
-abstract class IRepository<Ag extends IAgg<Id>, Id> {
-  // 返回Entity的类型
-  Type get entityType => Ag.runtimeType;
-}
-
-///
-/// 不使用 Identity
-abstract class IRepo<T, Id> {
+mixin IRepo<T, Id> {
   // 返回Entity的类型
   Type get entityType => T.runtimeType;
 }
 
-/// 增删改查
-/// 这不代表继承本类的Repo实现有且仅有以下方法
-abstract class ICrudRepository<E extends IAgg<Id>, Id>
-    extends IRepository<E, Id> {
-  /// 增
-  /// (当数据通过网络同步后, 返回值不一定与输入值相同, 因此这里的返回值不是[Unit]类型)
-  Future<E> create(E entity);
-
-  /// 查
-  Future<E> read(Id id);
-
-  /// 改
-  /// (当数据通过网络同步后, 返回值不一定与输入值相同, 因此这里的返回值不是[Unit]类型)
-  Future<E> update(E item);
-
-  /// 删
-  Future delete(Id id);
-}
-
 ///
-/// 不使用 Identity
-abstract class ICrudRepo<T, ID> extends IRepo<T, ID> {
+/// 同步仓库
+mixin ICrudRepo<T, Id> on IRepo<T, Id> {
   ///
   /// Saves a given entity. Use the returned instance for further operations as the save operation might have changed the
   /// entity instance completely.
@@ -53,11 +26,11 @@ abstract class ICrudRepo<T, ID> extends IRepo<T, ID> {
 
   ///
   /// Retrieves an entity by its id.
-  T findById(ID id);
+  T? findById(Id id);
 
   ///
   /// Returns whether an entity with the given id exists.
-  bool existsById(ID id);
+  bool existsById(Id id);
 
   ///
   /// Returns all instances of the type.
@@ -66,15 +39,15 @@ abstract class ICrudRepo<T, ID> extends IRepo<T, ID> {
   Iterable<T> findAll();
 
   ///
-  /// Returns all instances of the type {@code T} with the given IDs.
+  /// Returns all instances of the type {@code T} with the given Ids.
   /// <p>
-  /// If some or all ids are not found, no entities are returned for these IDs.
+  /// If some or all ids are not found, no entities are returned for these Ids.
   /// <p>
   /// Note that the order of elements in the result is not guaranteed.
   ///
   /// @return guaranteed to be not {@literal null}. The size can be equal or less than the number of given
   ///         {@literal ids}.
-  Iterable<T> findAllById(Iterable<ID> ids);
+  Iterable<T?> findAllById(Iterable<Id> ids);
 
   ///
   /// Returns the number of entities available.
@@ -87,7 +60,7 @@ abstract class ICrudRepo<T, ID> extends IRepo<T, ID> {
   ///
   /// @param id must not be {@literal null}.
   /// @throws IllegalArgumentException in case the given {@literal id} is {@literal null}
-  void deleteById(ID id);
+  void deleteById(Id id);
 
   ///
   /// Deletes a given entity.
@@ -97,14 +70,69 @@ abstract class ICrudRepo<T, ID> extends IRepo<T, ID> {
   void delete(T entity);
 
   ///
-  /// Deletes all instances of the type {@code T} with the given IDs.
+  /// Deletes all instances of the type {@code T} with the given Ids.
   ///
   /// @param ids must not be {@literal null}. Must not contain {@literal null} elements.
   /// @throws IllegalArgumentException in case the given {@literal ids} or one of its elements is {@literal null}.
   /// @since 2.5
-  void deleteAllById(Iterable<ID> ids);
+  void deleteAllById(Iterable<Id> ids);
 
   ///
   /// Deletes all entities managed by the repository.
   void deleteAll();
+}
+
+/// 异步仓库
+mixin IAsyncCrudRepo<Ag extends IAgg<Id>, Id> on IRepo<Ag, Id> {
+  // Returns the number of entities available
+  Future<int> count();
+
+  // Deletes a given entity.
+  Future<void> delete(Ag entity);
+
+  // if [entities] == null, Deletes all entities managed by the repository.
+  // else Deletes the given entities.
+  Future<void> deleteAll(Iterable<Ag>? entities);
+
+  // Deletes the given entities supplied by a Publisher.
+  Stream<void> deleteAllAsync(Stream<Ag> entityStream);
+
+  // Deletes all instances of the type T with the given Ids.
+  Future<void> deleteAllById(Iterable<Id> ids);
+
+  // Deletes the entity with the given id.
+  Future<void> deleteById(Id id);
+
+  // Deletes the entity with the given id supplied by a Publisher.
+  Future<void> deleteByIdAsync(Future<Id> id);
+
+  // Returns whether an entity with the given id exists.
+  Future<bool> existsById(Id id);
+
+  // Returns whether an entity with the given id, supplied by a Publisher, exists.
+  Future<bool> existsByIdAsync(Future<Id> id);
+
+  // Returns all instances of the type.
+  Stream<Ag> findAll();
+
+  // Returns all instances of the type Ag with the given Ids.
+  Stream<Ag?> findAllById(Iterable<Id> ids);
+
+  // Returns all instances of the type Ag with the given Ids supplied by a Publisher.
+  Stream<Ag?> findAllByIdAsync(Stream<Id> idStream);
+
+  // Retrieves an entity by its id.
+  Future<Ag?> findById(Id id);
+
+  // Retrieves an entity by its id supplied by a Publisher.
+  Future<Ag?> findByIdAsync(Future<Id> id);
+
+  // Saves a given entity.
+  Future<Ag> save(Ag entity);
+
+  // Saves all given entities.
+  Stream<Ag> saveAll(Iterable<Ag> entities);
+
+  // Saves all given entities.
+  Stream<Ag> saveAllAsync(Stream<Ag> entityStream);
 }
