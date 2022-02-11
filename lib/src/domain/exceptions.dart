2e55e11e281b82b33ceb82833dc06575e34f0244
huwentao <hu.wentao@outlook.com>
2022-02-12 00:36:40 +0800

    refactor exceptions.dart 调整异常继承关系, 继承自Dart:core Exception

--- lib/src/domain/exceptions.dart
+++ lib/src/domain/exceptions.dart
@@ -1,4 +1,4 @@
-class BaseException {
+class BaseException implements Exception {
   final String msg; // 直接向用户展示
   final String debugInfo; // 用于问题反馈，帮助定位BUG的信息，可以用stacktrace填充
 
@@ -9,28 +9,29 @@ class BaseException {
       'BaseException{msg: $msg, debugInfo: ${(debugInfo.length > maxInfoLen) ? '${debugInfo.substring(0, maxInfoLen)}...' : debugInfo}}';
 }
 
-/// 值对象验证出错
-class ValidationException extends BaseException {
-  ValidationException(String msg) : super(msg);
-}
-
 /// 参数错误
 class IllegalArgumentException extends BaseException {
   IllegalArgumentException(String msg) : super(msg);
 }
 
-/// 功能正在开发
-class FeatDeveloping extends BaseException {
-  FeatDeveloping(String msg) : super(msg);
+/// 参数错误- 找不到指定实例, 例如通过id找不到对应的车间
+class InstanceNotFoundException extends IllegalArgumentException {
+  InstanceNotFoundException(String msg) : super('找不到实例: $msg');
 }
 
+/// 参数错误- 值对象验证出错
+class ValidationException extends IllegalArgumentException {
+  ValidationException(String msg) : super(msg);
+}
+
+/// 状态错误
 /// 设置状态时发生错误, 例如仓库存储数量小于0
 /// 或者在用例中的前提条件错误, 例如某用例只有在生产线为idle时才能调用
 class InvalidStateException extends BaseException {
   InvalidStateException(String msg) : super('状态异常: $msg');
 }
 
-/// 找不到指定实例, 例如通过id找不到对应的车间
-class InstanceNotFoundException extends BaseException {
-  InstanceNotFoundException(String msg) : super('找不到实例: $msg');
+/// 状态错误- 功能正在开发
+class FeatDeveloping extends InvalidStateException {
+  FeatDeveloping(String msg) : super(msg);
 }
