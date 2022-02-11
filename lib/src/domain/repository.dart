9300b479700f6a7a123dd6f8bdf318eb3cff98ee
huwentao <hu.wentao@outlook.com>
2022-02-11 10:24:32 +0800

    remove aggregate.dart IAggregate

--- lib/src/domain/repository.dart
+++ lib/src/domain/repository.dart
@@ -8,7 +8,7 @@ import 'package:get_arch_core/get_arch_core.dart';
 ///
 /// 仓库抽象类
 /// 建议缩写为: Repo
-abstract class IRepository<Ag extends IAggregate<Id>, Id extends Identity> {
+abstract class IRepository<Ag extends IAgg<Id>, Id> {
   // 返回Entity的类型
   Type get entityType => Ag.runtimeType;
 }
@@ -22,7 +22,7 @@ abstract class IRepo<T, Id> {
 
 /// 增删改查
 /// 这不代表继承本类的Repo实现有且仅有以下方法
-abstract class ICrudRepository<E extends IAggregate<Id>, Id extends Identity>
+abstract class ICrudRepository<E extends IAgg<Id>, Id>
     extends IRepository<E, Id> {
   /// 增
   /// (当数据通过网络同步后, 返回值不一定与输入值相同, 因此这里的返回值不是[Unit]类型)
