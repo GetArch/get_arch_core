9de1f39038099f3797ad35e6207462fddf9b058b
胡文涛 <35894003+Hu-Wentao@users.noreply.github.com>
2022-10-19 23:33:04 +0800

    refactor dto.dart remove ::toJson

--- lib/src/interface/dto.dart
+++ lib/src/interface/dto.dart
@@ -2,12 +2,9 @@
 // Email : hu.wentao@outlook.com
 // Date  : 2020/6/12
 // Time  : 18:35
-/// Dto类实现该接口
+/// Data Transfer Object
 abstract class IDto {
   const IDto();
-
-  @Deprecated('toMap, Will be removed in GetArchCore v4.0')
-  Map<String, dynamic> toJson() => toMap();
 
   Map<String, dynamic> toMap();
 
